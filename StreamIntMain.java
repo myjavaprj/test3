77eb16d18d96b40a7f399e9ba96ed145e2aeca76
SerjAdm <SerjAdm@serg.ua>
2017-12-25 19:54:46 +0200

    v 2.0

--- StreamIntMain.java
+++ StreamIntMain.java
@@ -7,7 +7,7 @@ public class StreamIntMain {
 	public static void main(String[] args) {
 		Integer[] iArr = {1,2,13,4,5,61,7,8,91,10};
 		
-		System.out.println(Arrays.stream(iArr).skip(5).filter((p) -> p.intValue() % 2 == 0).count());
+		//System.out.println(Arrays.stream(iArr).skip(5).filter((p) -> p.intValue() % 2 == 0).count());
 		System.out.println(Arrays.stream(iArr).filter((p) -> p.intValue() % 3 == 0).findFirst().orElse(66613));
 	}
 
